2c2ec59dc95c641f56c20a15911c2636e8555909
Joe Yates <joe.g.yates@gmail.com>
2012-02-08 20:00:50 +0100

    Initial table_set test for pstgresql

---
+++ test/table_set/postgresql_test.cpp
@@ -0,0 +1,27 @@
+#include "test_helper.h"
+#include <active_record/connection/postgresql.h>
+
+class PostgresqlTableSetTest : public ::testing::Test {
+ protected:
+  virtual void SetUp() {
+    postgresql_shell_create_database( "active_record_postgresql_test", "template1", Q(PG_USER) );
+    connection.connect( options
+                        ( "database", "active_record_postgresql_test" )
+                        ( "username", Q(PG_USER) ) );
+   }
+  virtual void TearDown() {
+    connection.disconnect();
+    postgresql_shell_drop_database( "active_record_postgresql_test", Q(PG_USER) );
+  }
+ protected:
+  PostgresqlConnection connection;
+};
+
+TEST_F( PostgresqlTableSetTest, CreateTable ) {
+  Table td( &connection, "people" );
+
+  TableSet::create_table( td );
+
+  assert_postgresql_table_exists( database_name );
+}
+
